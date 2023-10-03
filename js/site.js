8cbad08ca4aa306b760798c3a000f907a11212e9
Nidia Guadarrama <47833881+Aly218@users.noreply.github.com>
2023-10-02 19:33:21 -0500

    update

---
+++ js/site.js
@@ -0,0 +1,11 @@
+// jQuery 3.x-style ready event and locally scoped $
+jQuery(function($) {
+  $('html').removeClass('nojs');
+  $('html').addClass('hasjs');
+});
+
+$(document).ready(function () {
+            $("#submit").click(function () {
+                alert("Thank you for signing up!");
+            });
+        });
